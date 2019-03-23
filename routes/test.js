b87c871e05be5f8ecdbf95f6bc52c8fd9b9fed40
Victoria1349 <grimbug13@gmail.com>
2019-03-23 20:06:30 +0100

    test

--- routes/test.js
+++ routes/test.js
@@ -2,26 +2,27 @@ const Caregiver = require('../models/caregiver');
 const Pid = require('../models/pid');
 
 exports.post = function (req, res) {
-    var newCaregiver = new Caregiver();
-    newCaregiver.email = 'req.body.email';
-    newCaregiver.password = 'createHash(req.body.password)';
+    // var newCaregiver = new Caregiver();
+    // newCaregiver.email = 'req.body.email';
+    // newCaregiver.password = 'createHash(req.body.password)';
 
-    var newPid= new Pid();
-    newPid.email = 'req.body.email';
-    newPid.password = 'createHash(req.body.password)';
+    // var newPid= new Pid();
+    // newPid.email = 'req.body.email';
+    // newPid.password = 'createHash(req.body.password)';
 
-    newCaregiver.save(function (err) {
-        if (err) {
-            res.status(403).send(err);
-            throw err;
-        }
-        newPid.save(function (err) {
-            if (err) {
-                res.status(403).send(err);
-                throw err;
-            }
-            res.status(200).send('dada');
-        });
+    // newCaregiver.save(function (err) {
+    //     if (err) {
+    //         res.status(403).send(err);
+    //         throw err;
+    //     }
+    //     newPid.save(function (err) {
+    //         if (err) {
+    //             res.status(403).send(err);
+    //             throw err;
+    //         }
+    //         res.status(200).send('dada');
+    //     });
   
-    });
+    // });
+    res.status(200).send('dada');
 }
