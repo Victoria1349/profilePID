843a61a90b364d3b6303c1e5aa7bbbb98aaca3b4
Victoria1349 <grimbug13@gmail.com>
2019-04-23 17:28:56 +0200

    added swager

---
+++ routes/api/v1/get_answers.js
@@ -0,0 +1,6 @@
+const Profile = require('../../../models/profile');
+const Question = require('../../../models/question');
+
+exports.post = async function (req, res) {
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -34,25 +34,49 @@ res.send(swaggerSpec);
 });
 
 /**
- * @swagger
- * /api/users:
- *   post:
- *     tags:
- *       - users
- *     description: Creates a new user
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: tarun
- *         description: user object
- *         in: body
- *         required: true
- *         schema:
- *           $ref: '#/definitions/user'
- *     responses:
- *       200:
- *         description: Successfully created
- */
+    * @swagger
+    * /api/v1/applications/moneygame/get/currency:
+    *   post:
+    *     tags:
+    *       - ""
+    *     summary: "Get currency for MoneyGame"
+    *     description: ""
+    *     produces:
+    *       - application/json
+    *     parameters:
+    *     - name: "x-access-token"
+    *       in: "header"
+    *       description: "PID token"
+    *       required: true
+    *       type: "string"
+    *     responses:
+    *       200:  
+    *        description: Information about currency for MoneyGame
+    *        examples:
+    *           application/json: { 
+    *           banknotes: [{ "count": 5, "image": "test-ait.herokuapp.com/system_images/currency/euro/banknotes/5.png" },
+    *           { "count": 10, "image": "test-ait.herokuapp.com/system_images/currency/euro/banknotes/10.png" }],
+    *           coins: [{ "count": 0.01, "image": "test-ait.herokuapp.com/system_images/currency/euro/coins/0,01.png" }], 
+    *           currency: "euro"  
+    *           }
+    *       401:
+    *         description: Invalid data entered
+    *         examples:
+    *           application/json: 
+    *            {  
+    *              errors:
+    *              [
+    *               {
+    *                "id": 1, "title": Required fields are not filled, "detail": "Empty token value"
+    *                },{
+    *                "id": 2, "code": token-Invalid, "title": Invalid data entered, "detail": "Invalid token entered, or token expired"
+    *                },{
+    *                "id": 2, "token": token-Invalid, "title": Invalid data entered, "detail": "The PID with the token entered has been deleted."
+    *                }         
+    *              ]
+    *            }
+    *           
+    */
+router.post('/api/v1/get-answers', require('./api/v1/get_answers').post);
 
- 
 module.exports = router;
